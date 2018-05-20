bae32d3eeb348b08b776ca859c153eecab4be161
hally9k <hally9k@gmail.com>
2018-05-20 20:54:19 +1200

    Just need to fix the relay compiler typescript support

--- src/component/index.tsx
+++ src/component/index.tsx
@@ -1,14 +1,25 @@
 import * as React from 'react'
 import { hot } from 'react-hot-loader'
-import SignUp from './sign-up'
-
+import SignUp from 'component/auth/sign-up'
+import Login from 'component/auth/login'
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom'
+import Navigation from 'component/core/navigation'
 import './index.scss'
 
 export class Root extends React.Component {
 	public render() {
 		return (
 			<div className="app">
-				<SignUp />
+				<Router>
+					<React.Fragment>
+						<Navigation />
+						<Switch>
+							<Route exact path={'/'} component={() => <p>Home</p>} />
+							<Route exact path={'/signup'} component={SignUp} />
+							<Route exact path={'/login'} component={Login} />
+						</Switch>
+					</React.Fragment>
+				</Router>
 			</div>
 		)
 	}
