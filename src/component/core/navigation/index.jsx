b8a473c16f5d57f4428321514b9d2229dd865842
Pulasthi Bandara <pulasthi@vitaler.co>
2018-06-06 23:44:15 +1200

    render route components

--- src/component/core/navigation/index.jsx
+++ src/component/core/navigation/index.jsx
@@ -1,10 +1,20 @@
+// @flow
 import * as React from 'react'
 import { Link } from 'found'
 
-export default () => (
-	<div className="navigation">
-		<Link to="/">Home</Link>
-		<Link to="/signup">Signup</Link>
-		<Link to="/login">Login</Link>
-	</div>
-)
+type Props = {
+    children: React.Node
+}
+
+export default function Navigation(props: Props): React.Node {
+    return (
+        <React.Fragment>
+            <div className="navigation">
+                <Link to="/">Home</Link>
+                <Link to="/signup">Signup</Link>
+                <Link to="/login">Login</Link>
+            </div>
+            {props.children}
+        </React.Fragment>
+    )
+}
