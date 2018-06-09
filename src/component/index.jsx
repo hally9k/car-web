aeec42b8ace3c972641883e37f46b914e6e2c7d9
hally9k <hally9k@gmail.com>
2018-06-10 10:53:28 +1200

    Fix linting and flow resolution

--- src/component/index.jsx
+++ src/component/index.jsx
@@ -1,25 +1,27 @@
+// @flow
 import * as React from 'react'
-import { hot } from 'react-hot-loader'
-import Navigation from 'component/core/navigation'
 import { queryMiddleware, HashProtocol } from 'farce'
 import { createRender, createFarceRouter } from 'found'
 import { Resolver } from 'found-relay/lib/modern'
+import { hot } from 'react-hot-loader'
+
 import environment from 'environment'
 import routes from 'routes'
+
 import './index.scss'
 
-const Router = createFarceRouter({
-	historyProtocol: new HashProtocol(),
-	historyMiddlewares: [queryMiddleware],
-	routeConfig: routes,
+const Router: * = createFarceRouter({
+    historyProtocol: new HashProtocol(),
+    historyMiddlewares: [queryMiddleware],
+    routeConfig: routes,
 
-	render: createRender({})
+    render: createRender({})
 })
 
 export class Root extends React.Component<*, *> {
-	render() {
-		return <Router resolver={new Resolver(environment)} />
-	}
+    render(): React.Node {
+        return <Router resolver={new Resolver(environment)} />
+    }
 }
 
 export default hot(module)(Root)
